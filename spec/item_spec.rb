5cce8038de60e07048af7a4e899932e35f25769e
Amanda Ross <amikaross@gmail.com>
2022-09-26 10:19:09 -0600

    Write test for close_bidding in item_spec

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -7,6 +7,7 @@ RSpec.describe Item do
     @item1 = Item.new("Chalkware Piggy Bank")
     @attendee1 = Attendee.new({name: "Megan", budget: "$50"})
     @attendee2 = Attendee.new({name: "Bob", budget: "$75"})
+    @attendee3 = Attendee.new({name: "Mike", budget: "$100"})
   end
 
   describe "#initialize" do 
@@ -36,6 +37,17 @@ RSpec.describe Item do
       @item1.add_bid(@attendee2, 20)
       @item1.add_bid(@attendee1, 22)
       expect(@item1.current_high_bid).to eq(22)
+    end
+  end
+
+  describe "#close_bidding" do 
+    it "makes it so bids are no longer able to be added" do 
+      @item1.add_bid(@attendee2, 20)
+      @item1.add_bid(@attendee1, 22)
+      expect(@item1.bids).to eq({@attendee2 => 20, @attendee1 => 22})
+      @item1.close_bidding
+      @item1.add_bid(@attendee3, 70)
+      expect(@item1.bids).to eq({@attendee2 => 20, @attendee1 => 22})
     end
   end
 end
