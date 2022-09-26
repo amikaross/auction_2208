a2cfb6e27ae1a1bcfa61480766fd69a29f501d29
Amanda Ross <amikaross@gmail.com>
2022-09-26 09:15:01 -0600

    Remove unnecesary Items in attendee_spec

--- spec/attendee_spec.rb
+++ spec/attendee_spec.rb
@@ -4,8 +4,6 @@ require "./lib/attendee"
 
 RSpec.describe Attendee do 
   before(:each) do 
-    @item1 = Item.new("Chalkware Piggy Bank")
-    @item2 = Item.new("Bamboo Picture Frame")
     @attendee = Attendee.new({name: "Megan", budget: "$50"})
   end
 
