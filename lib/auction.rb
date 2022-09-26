99a60640195a06ad6015b2d2d1b50ba5400a723a
Amanda Ross <amikaross@gmail.com>
2022-09-26 09:44:29 -0600

    Add unpopular_items and potential_revenue to Auction class

--- lib/auction.rb
+++ lib/auction.rb
@@ -1,6 +1,6 @@
 class Auction 
   attr_reader :items 
-  
+
   def initialize
     @items = []
   end
@@ -11,5 +11,13 @@ class Auction
 
   def item_names
     @items.map { |item| item.name }
+  end
+
+  def unpopular_items
+    @items.select { |item| item.bids == {} }
+  end
+
+  def potential_revenue
+    (@items - unpopular_items).sum { |item| item.current_high_bid }
   end
 end
